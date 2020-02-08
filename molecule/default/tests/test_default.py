bcd447e76022ecfd54571051a0f7edfa3b9b452e
Gabriel Suarez <gabriel.suarez@solarwinds.com>
2020-02-08 18:23:01 +0000

    Added simple tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_logrotate_files(host):
+    m = host.file('/etc/logrotate.d/messages')
+    f = host.file('/etc/logrotate.d/httpd')
+    n = host.file('/etc/logrotate.d/httpd')
 
+    assert m.exists
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert n.exists
